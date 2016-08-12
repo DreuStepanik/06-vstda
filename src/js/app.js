8f8c13e0f8a80dc406b712b2650baa6f99f49f70
Dreu Stepanik <dreuezekiel@gmail.com>
2016-08-12 13:49:00 -0700

    Revisitied the code, added style and detail to the user interface

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,11 @@
-
+//Initiate an angular module 
+//Give it a name to be referred to within the HTML page.
 angular.module("todoListApp", [])
-
+//Declare a controller for the module
+//Give it a name to be referred to within the HTML page.
+//Initiate the function of $scope within the controller
 .controller('mainCtrl', function($scope) {
+//Create an empty array to hold the variables and objects
 	$scope.todos = [];
 
 	$scope.blankTodo = {
@@ -10,19 +14,11 @@ angular.module("todoListApp", [])
 	};
 
 	$scope.addTodo = function() {
+//In order to give value to the 'blankTodo' there needs to be an angular copy
 		var copyOfBlankTodo = angular.copy($scope.blankTodo); 
 		$scope.todos.push(copyOfBlankTodo);
 		$scope.orderMyTodos = angular.orderMyTodos;
 		$scope.blankTodo={};
 	};
-	 function Test($scope){
-
-        if($scope.task + $scope.priority ==''){
-            $scope.output="true";
-        }
-        else{
-            $scope.output="false";
-        }
-    }
 
 });
